535f4ee8c3a31f7fb723f58f37ddf86d6e344f7b
simon kuria <qushrickman@gmail.com>
2025-12-07 20:24:44 +0300

    addition of  Custom Views and Generic Views in Django REST Framework 4th try

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,58 +1,44 @@
-from rest_framework import generics, permissions, mixins
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
 
-# -------------------------------
-# LIST VIEW
-# -------------------------------
 class BookListView(mixins.ListModelMixin,
                    generics.GenericAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
 
-# -------------------------------
-# DETAIL VIEW
-# -------------------------------
 class BookDetailView(mixins.RetrieveModelMixin,
                      generics.GenericAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
 
 
-# -------------------------------
-# CREATE VIEW
-# -------------------------------
 class BookCreateView(mixins.CreateModelMixin,
                      generics.GenericAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
-    def perform_create(self, serializer):
-        serializer.save()
 
-
-# -------------------------------
-# UPDATE VIEW
-# -------------------------------
 class BookUpdateView(mixins.UpdateModelMixin,
                      generics.GenericAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
@@ -60,18 +46,12 @@ class BookUpdateView(mixins.UpdateModelMixin,
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
-    def perform_update(self, serializer):
-        serializer.save()
 
-
-# -------------------------------
-# DELETE VIEW
-# -------------------------------
 class BookDeleteView(mixins.DestroyModelMixin,
                      generics.GenericAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
