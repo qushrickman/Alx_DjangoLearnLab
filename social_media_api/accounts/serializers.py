945357139778317702a89f4a34d19dfadbcd529a
simon kuria <qushrickman@gmail.com>
2025-12-20 08:07:16 +0300

    re do moving file into alx hub with changes tothe serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import User
+
+User = get_user_model()
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -15,21 +17,29 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
-            bio=validated_data.get('bio', '')
         )
+        user.bio = validated_data.get('bio', '')
+        user.save()
+
         Token.objects.create(user=user)
         return user
 
+
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        user = authenticate(**data)
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
         if not user:
             raise serializers.ValidationError("Invalid credentials")
+
         token, _ = Token.objects.get_or_create(user=user)
         return token.key
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
